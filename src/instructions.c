ea88132966f7afe6de4f9a2b65dd84605fe4c0e2
Rodrigo Lopez <68488284+rlopez44@users.noreply.github.com>
2021-01-18 22:10:59 -0600

    Implement the twelve INC instructions

--- src/instructions.c
+++ src/instructions.c
@@ -1116,6 +1116,115 @@ static void ldh(gb_cpu *cpu, gb_instruction *inst)
     }
 }
 
+// the increment instruction
+static void inc(gb_cpu *cpu, gb_instruction *inst)
+{
+    switch (inst->op1)
+    {
+        case REG_A:
+        {
+            // store old value for carry flag check
+            uint8_t old_a = (cpu->reg->a)++;
+
+            set_zero_flag(cpu->reg, cpu->reg->a == 0);
+            set_subtract_flag(cpu->reg, 0);
+            set_half_carry_flag(cpu->reg, (old_a & 0xf) + 1 > 0xf);
+            break;
+        }
+
+        case REG_B:
+        {
+            uint8_t old_b = (cpu->reg->b)++;
+
+            set_zero_flag(cpu->reg, cpu->reg->b == 0);
+            set_subtract_flag(cpu->reg, 0);
+            set_half_carry_flag(cpu->reg, (old_b & 0xf) + 1 > 0xf);
+            break;
+        }
+
+        case REG_C:
+        {
+            uint8_t old_c = (cpu->reg->c)++;
+
+            set_zero_flag(cpu->reg, cpu->reg->c == 0);
+            set_subtract_flag(cpu->reg, 0);
+            set_half_carry_flag(cpu->reg, (old_c & 0xf) + 1 > 0xf);
+            break;
+        }
+
+        case REG_D:
+        {
+            uint8_t old_d = (cpu->reg->d)++;
+
+            set_zero_flag(cpu->reg, cpu->reg->d == 0);
+            set_subtract_flag(cpu->reg, 0);
+            set_half_carry_flag(cpu->reg, (old_d & 0xf) + 1 > 0xf);
+            break;
+        }
+
+        case REG_E:
+        {
+            uint8_t old_e = (cpu->reg->e)++;
+
+            set_zero_flag(cpu->reg, cpu->reg->e == 0);
+            set_subtract_flag(cpu->reg, 0);
+            set_half_carry_flag(cpu->reg, (old_e & 0xf) + 1 > 0xf);
+            break;
+        }
+
+        case REG_H:
+        {
+            uint8_t old_h = (cpu->reg->h)++;
+
+            set_zero_flag(cpu->reg, cpu->reg->h == 0);
+            set_subtract_flag(cpu->reg, 0);
+            set_half_carry_flag(cpu->reg, (old_h & 0xf) + 1 > 0xf);
+            break;
+        }
+
+        case REG_L:
+        {
+            uint8_t old_l = (cpu->reg->l)++;
+
+            set_zero_flag(cpu->reg, cpu->reg->l == 0);
+            set_subtract_flag(cpu->reg, 0);
+            set_half_carry_flag(cpu->reg, (old_l & 0xf) + 1 > 0xf);
+            break;
+        }
+
+        case PTR_HL:
+        {
+            uint16_t addr = read_hl(cpu->reg);
+            uint8_t old_val = read_byte(cpu->bus, addr);
+            write_byte(cpu->bus, addr, old_val + 1);
+
+            set_zero_flag(cpu->reg, old_val + 1 == 0);
+            set_subtract_flag(cpu->reg, 0);
+            set_half_carry_flag(cpu->reg, (old_val & 0xf) + 1 > 0xf);
+            break;
+        }
+
+        case REG_BC:
+            write_bc(cpu->reg, read_bc(cpu->reg) + 1);
+            break;
+
+        case REG_DE:
+            write_de(cpu->reg, read_de(cpu->reg) + 1);
+            break;
+
+        case REG_HL:
+            write_hl(cpu->reg, read_hl(cpu->reg) + 1);
+            break;
+
+        case REG_SP:
+            ++(cpu->reg->sp);
+            break;
+
+        default: // shouldn't get here
+            break;
+    }
+}
+
 /* updates the program counter after instruction execution
  * returns the number of m-cycles elapsed during instruction execution
  */
@@ -1150,6 +1259,7 @@ uint8_t execute_instruction(gb_cpu *cpu)
             break;
 
         case INC:
+            inc(cpu, &inst);
             break;
 
         case DEC:
