bf8846b80b7bc590a7d561de54154b30c0367b96
Rodrigo Lopez <68488284+rlopez44@users.noreply.github.com>
2021-02-15 23:11:45 -0600

    Load the ROM file into the emulator given the file path
    Make the emulator use the ROM file path passed in to load
    this ROM into the emulator during initialization.

--- src/main.c
+++ src/main.c
@@ -5,33 +5,85 @@
 #include "cboy/cartridge.h"
 #include "cboy/memory.h"
 
-int main(int argc, const char *argv[])
+/* Initializes the emulator and loads the
+ * ROM file into the emulator.
+ *
+ * If initialization fails then NULL is
+ * returned and an error message is printed
+ * out. Otherwise, the initialized gameboy
+ * with the ROM loaded is returned.
+ */
+static gameboy *init_emulator(const char *rom_file_path)
 {
-    if (argc != 2)
+    // open the ROM file to load it into the emulator
+    FILE *rom_file = fopen(rom_file_path, "rb");
+
+    if (rom_file == NULL)
     {
-        printf("CBoy - A Game Boy Emulator\nUsage: cboy <rom_file_path>\n");
+        fprintf(stderr, "Failed to open the ROM file (incorrect path?)\n");
+        return NULL;
     }
-    else // TODO: actually do stuff
+
+    gameboy *gb = init_gameboy();
+
+    if (gb == NULL)
     {
-        gameboy *gb = init_gameboy();
+        fprintf(stderr, "Not enough memory to initialize the emulator\n");
+        fclose(rom_file);
+        return NULL;
+    }
 
-        if (gb == NULL)
+    // load the ROM file into the emulator
+    ROM_LOAD_STATUS load_status = load_rom(gb->cart, rom_file);
+    fclose(rom_file);
+
+    if (load_status != ROM_LOAD_SUCCESS)
+    {
+        if (load_status == MALFORMED_ROM)
         {
-            printf("Failed to initialize the emulator\n");
-            return 1;
+            fprintf(stderr, "ROM file is incorrectly formatted\n");
         }
-        printf("CBoy - A Game Boy Emulator\n");
-
-        // should be 512, since this is default value during initialization
-        printf("Number of banks in the ROM: %d\n", gb->cart->num_banks);
-
-        // should be 1, since the boot ROM is disabled during initialization
-        printf("Boot ROM disabled bit: %d\n", read_byte(gb->memory, 0xff50));
-
-        // initial register contents
-        print_registers(gb->cpu);
+        else if (load_status == ROM_LOAD_ERROR)
+        {
+            fprintf(stderr, "Failed to load the ROM into the emulator (I/O or memory error)\n");
+        }
 
         free_gameboy(gb);
+        return NULL;
     }
+
+    // init successful and ROM loaded
+    return gb;
+}
+
+int main(int argc, const char *argv[])
+{
+    printf("CBoy - A Game Boy Emulator\n"
+           "--------------------------\n");
+
+    if (argc != 2)
+    {
+        fprintf(stderr, "Usage: cboy <rom_file_path>\n");
+        return 1;
+    }
+
+    // TODO: actually do stuff
+    gameboy *gb = init_emulator(argv[1]);
+
+    if (gb == NULL)
+    {
+        return 1;
+    }
+
+    // should be 512, since this is default value during initialization
+    printf("Number of banks in the ROM: %d\n", gb->cart->num_banks);
+
+    // should be 1, since the boot ROM is disabled during initialization
+    printf("Boot ROM disabled bit: %d\n", read_byte(gb->memory, 0xff50));
+
+    // initial register contents
+    print_registers(gb->cpu);
+
+    free_gameboy(gb);
     return 0;
 }
