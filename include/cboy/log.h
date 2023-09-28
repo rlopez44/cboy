43a55cf915d7368e304ffcf890ee3d2d9181bd83
Rodrigo Lopez <68488284+rlopez44@users.noreply.github.com>
2023-09-28 00:04:15 -0500

    Wrap logging macros in do-while-false blocks
    This silences the `-Wempty-body` previously generated when
    using the macros. Also forces the use of a semicolon after
    the macro without introducing unexpected results after
    expansion.

--- include/cboy/log.h
+++ include/cboy/log.h
@@ -5,12 +5,12 @@
 #include <SDL_log.h>
 #include "cboy/gameboy.h"
 
-#define ENABLE_DEBUG_LOGS() SDL_LogSetPriority(SDL_LOG_CATEGORY_APPLICATION, SDL_LOG_PRIORITY_VERBOSE)
+#define ENABLE_DEBUG_LOGS() do {SDL_LogSetPriority(SDL_LOG_CATEGORY_APPLICATION, SDL_LOG_PRIORITY_VERBOSE);} while (0)
 
 /* credit to https://github.com/sysprog21/jitboy for these macros */
-#define LOG_DEBUG(...) SDL_LogDebug(SDL_LOG_CATEGORY_APPLICATION, __VA_ARGS__)
-#define LOG_ERROR(...) SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, __VA_ARGS__)
-#define LOG_INFO(...)  SDL_LogInfo(SDL_LOG_CATEGORY_APPLICATION, __VA_ARGS__)
+#define LOG_DEBUG(...) do {SDL_LogDebug(SDL_LOG_CATEGORY_APPLICATION, __VA_ARGS__);} while (0)
+#define LOG_ERROR(...) do {SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, __VA_ARGS__);} while (0)
+#define LOG_INFO(...)  do {SDL_LogInfo(SDL_LOG_CATEGORY_APPLICATION, __VA_ARGS__);} while (0)
 
 // dump the Game Boy's memory contents
 void dump_memory(gameboy *gb);
@@ -20,9 +20,9 @@ void print_registers(gameboy *gb);
 
 #else
 #include <stdio.h>
-#define LOG_DEBUG(...)
-#define LOG_ERROR(...) fprintf(stderr, __VA_ARGS__)
-#define LOG_INFO(...)  printf(__VA_ARGS__)
+#define LOG_DEBUG(...) do {} while (0)
+#define LOG_ERROR(...) do {fprintf(stderr, __VA_ARGS__);} while (0)
+#define LOG_INFO(...)  do {printf(__VA_ARGS__);} while (0)
 
 #endif /* DEBUG */
 
